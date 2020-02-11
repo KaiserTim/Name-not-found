d290d27e131160550410bea83209634dbf65db74
lulu184 <luisa.schuhmacher@hhu.de>
2020-02-11 15:37:34 +0100

    gitignore

--- QuadTree.py
+++ QuadTree.py
@@ -2,7 +2,7 @@ import h5py
 
 class ObjectMask:
 
-    quadtree_root = None
+    quad_root = None
 
     def __init__(self, path):
         self.path = path
@@ -43,32 +43,31 @@ class ObjectMask:
     def hdf5_to_json(self, file):
         pass
 
-class Node:
-    coords = None
+class Quad_node:
+    """
+    coordinates: topright, bottomright, bottomleft, topleft
+    children: northeast, southeast, southwest, northwest
 
-    # children: northeast, southeast, southwest, northwest
-    ne = None
-    se = None
-    sw = None
-    nw = None
-
-    def __init__(self, coords, ne, se, sw, nw):
-        self.coords = coords
-
-        self.ne = ne
-        self.se = se
-        self.sw = sw
-        self.nw = nw
-
-class Coordinates:
-    # topright, bottomright, bottomleft, topleft
+    """
     tr = None
     br = None
     bl = None
     tl = None
 
-    def __init__(self, tr, br, bl, tl):
+    ne = None
+    se = None
+    sw = None
+    nw = None
+
+
+
+    def __init__(self, tr, br, bl, tl, ne=None, se=None, sw=None, nw=None):
         self.tr = tr
         self.br = br
         self.bl = bl
         self.tl = tl
+
+        self.ne = ne
+        self.se = se
+        self.sw = sw
+        self.nw = nw
