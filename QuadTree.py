85fd307c016f459042543ba3d71fe43304c6b354
KaiserTim <kaiserti@hotmail.de>
2020-02-11 13:59:39 +0100

    Empty Class, only defined desired functions

---
+++ QuadTree.py
@@ -0,0 +1,33 @@
+
+
+class QuadTree:
+
+    def __init__(self):
+        # Constructor, no QuadTree yet
+        pass
+
+    def read_json(self, file):
+        # Construct the QuadTree here
+        pass
+
+    def read_hdf5(self, file):
+        # Construct the QuadTree here
+        pass
+
+    def check(self, obj, list):
+        pass
+
+    def extract(self, obj):
+        pass
+
+    def output_json(self, obj):
+        pass
+
+    def output_hdf5(self, obj):
+        pass
+
+    def json_to_hdf5(self, file):
+        pass
+
+    def hdf5_to_json(self, file):
+        pass
