e9b5f622357fc53ca1c4dad8571205c903795861
KaiserTim <kaiserti@hotmail.de>
2020-02-13 15:54:37 +0100

    final lecture commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import QuadTree
 import time
 
@@ -9,20 +8,21 @@ if __name__ == '__main__':
     path = "data/hdf5_image/B01_0361_annotations_si_spacing1.hdf5"
     quadtree = QuadTree.ObjectMask(path=path)
     quadtree.read()
-    print("Tree constructed", time.time() - start_time)
-    n, m = 10, 10
-    x_offset = 1000
-    y_offset = 1000
+    # Generate a rectangle of indices
+    print("Tree constructed after", time.time() - start_time, "s")
+    n, m = 10000, 1000
+    x_offset = quadtree.data.shape[0]//2
+    y_offset = quadtree.data.shape[1]//2
     a = []
     for i in range(x_offset, x_offset + m):
         for j in range(y_offset, y_offset + n):
             a.append(i)
     b = list(range(x_offset, x_offset + n))*m
-    #points = list(zip(b,a))
-    points = np.load("B01_0361_annotations_si_spacing1.npz", allow_pickle=True)["arr_0"]
+    points = list(zip(b,a))
+
+    # points = np.load("points/B01_0361_annotations_si_spacing1.npz", allow_pickle=True)["arr_0"]
     print(points[:10])
 
-
-    print(quadtree.check(obj_nr=1, points=points))
-    print("Points checked", time.time()-start_time)
+    print(np.any(quadtree.check(obj_nr=1, points=points)))
+    print("Points checked after", time.time()-start_time, "s")
 
