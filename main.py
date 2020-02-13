6606d9aae7ef4c9a72bbea1506d6a0bcaf3664b0
lulu184 <luisa.schuhmacher@hhu.de>
2020-02-13 15:27:25 +0100

    tried main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 import QuadTree
 
 if __name__ == '__main__':
     path = "./data/hdf5_image/B01_0361_annotations_si_spacing1.hdf5"
     quadtree = QuadTree.ObjectMask(path=path)
     quadtree.read()
-    points = [(5,2)]
+    points = np.load("B01_0361_annotations_si_spacing1.npz", allow_pickle=True)["arr_0"]
     print(quadtree.check(obj_nr=1, points=points))
